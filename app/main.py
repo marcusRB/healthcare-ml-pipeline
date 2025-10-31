88e3e7c96d495378d3e1b9dfd7f666bf25fbdc88
marcusRB <mrussorb@gmail.com>
2025-10-31 11:24:43 +0100

    fix init application

--- app/main.py
+++ app/main.py
@@ -55,6 +55,6 @@ def model_health():
         "target": predictor.target
     }
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
